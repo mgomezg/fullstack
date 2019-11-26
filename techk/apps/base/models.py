357755cf77a7c53ea80d98e33da706f5cdedc4f5
dfloresc <dt.floresc>
2019-11-26 12:51:42 -0600

    Base Models Creation

--- techk/apps/base/models.py
+++ techk/apps/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40, null=False)
+
+class Book(model.Model):
+    title = models.CharField(max_length=120, null=False)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.FloatField(null=False)
+    stock = models.IntegerField(null=False)
+    upc = models.CharField(max_length=20, null=False)
